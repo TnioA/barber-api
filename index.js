f16cd5b34918afa355d1c2622bb815fa9ba983ea
tnioa <hortanio@hotmail.com>
2021-11-27 01:15:45 -0300

    .

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const JWT_PRIVATE_KEY = 'myprivatetoken';
 const ValidateToken = (req, res, next) => {
     var authorization = req.headers['authorization'];
     if(!authorization || !authorization.includes('Bearer '))
-        return res.status(401).json({ success: false, data: null, error: 'token não informado'});
+      return res.status(401).json({ success: false, data: null, error: 'token não informado'});
 
     var token = authorization.split(' ')[1];
     verify(token, JWT_PRIVATE_KEY, (err, decode)=> {
         if(!err) {
-            next();
+          next();
         } else {
-            return res.status(401).json({ success: false, data: null, error: err.name});
+          return res.status(401).json({ success: false, data: null, error: err.name});
         }
     }); 
 }
@@ -29,7 +29,7 @@ app.post('/api/checktoken', ValidateToken, (req, res) => {
     var authorization = req.headers['authorization'];
     var token = authorization.split(' ')[1];
     var user = decode(token);
-    
+
     return res.json({ success: true, data: { token: token, avatar: user.data.avatar}, error: null });
 });
 
@@ -37,15 +37,15 @@ app.post('/api/signin', (req, res) => {
     var response = [];
     
     if(!req.body.email || !req.body.password)
-        return res.json({ success: false, data: null, error: 'Usuário não encontrado.' });
+      return res.json({ success: false, data: null, error: 'Usuário não encontrado.' });
 
     data.users.map(x=> {
-        if(x.email === req.body.email.toString() && x.password === req.body.password.toString())
-            response.push(x);
+      if(x.email === req.body.email.toString() && x.password === req.body.password.toString())
+          response.push(x);
     });
     
     if(response.length === 0)
-        return res.json({ success: false, data: null, error: 'Usuário não encontrado.' });
+      return res.json({ success: false, data: null, error: 'Usuário não encontrado.' });
 
     var token = sign({data: data.users[0]}, JWT_PRIVATE_KEY, {expiresIn : '30m'});
     return res.json({ success: true, data: { token: token, avatar: response[0].avatar }, error: null });
@@ -67,14 +67,14 @@ app.get('/api/getbarbers', (req, res) => {
 app.get('/api/getbarber', (req, res) => {
     var response = [];
     data.barbers.map(x=> {
-        if(x.id === parseInt(req.query.id))
-            response.push(x);
+      if(x.id === parseInt(req.query.id))
+          response.push(x);
     });
     
     return res.json({ 
-        success: response.length > 0,
-        data: response.length > 0 ? response[0] : null,
-        error: response.length > 0 ? null : 'Barbeiro não encontrado.'
+      success: response.length > 0,
+      data: response.length > 0 ? response[0] : null,
+      error: response.length > 0 ? null : 'Barbeiro não encontrado.'
     });
 });
 
@@ -84,6 +84,23 @@ app.post('/api/favoriteBarber', ValidateToken, (req, res) => {
 
 app.post('/api/setappointment', ValidateToken, (req, res) => {
   return res.json({ success: true, data: null, error: null });
+});
+
+app.get('/api/getappointments', ValidateToken, (req, res) => {
+  var authorization = req.headers['authorization'];
+  var token = authorization.split(' ')[1];
+  var user = decode(token);
+
+  var response = [];
+  data.users.map(x=> {
+    if(x.email === user.data.email.toString())
+      response.push(x);
+  });
+
+  if(response.length === 0)
+    return res.json({ success: false, data: null, error: 'Usuário não encontrado.' });
+
+  return res.json({ success: true, data: response[0].appointments, error: null });
 });
 
 const port = app.get('port');
