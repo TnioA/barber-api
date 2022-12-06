242e8fd11d3b85601556f1b9aeba00f118335c74
Hortanio Rocha Silva <hortanio.silva@dotzmkt.com.br>
2022-12-06 13:04:32 -0300

    Correção de versão

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./App/App.js')();
-const port = app.get('port');
-
-// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
-});
