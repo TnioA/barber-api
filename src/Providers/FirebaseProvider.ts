b93865a9f9a998000e48954c23abf61c65a0fa0c
tnioa <hortanio@gmail.com>
2024-06-27 22:53:30 -0300

    Correção de smells

--- src/Providers/FirebaseProvider.ts
+++ src/Providers/FirebaseProvider.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
-const serviceAccount = require("../../barberapp-e511c-firebase-adminsdk-3drvt-08ec6ef6c3.json");
+const serviceAccount = require("../../key.json");
 
 export default new class FirebaseProvider {
     public connection: any = null;
